26fc15af6f3bd7482089282718f1c5ae3fc88572
Leslie Patino <lesliejpatino@gmail.com>
2022-01-26 18:02:14 -0800

    created manager class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,21 @@
 const Employee = require("../lib/Employee");
 
-test("testing to see if we can get the employees name", () => {
+test("testing employees name", () => {
     const nameInstance = new Employee("Taylor") 
     expect(nameInstance.getName()).toBe("Taylor")
 })
 
+// test("testing employees ID", () => {
+//     const idInstance = new Employee("1") 
+//     expect(idInstance.getId()).toBe("1")
+// })
+
+// test("testing employees email", () => {
+//     const emailInstance = new Employee("leslie@gmail.com") 
+//     expect(emailInstance.getId()).toBe("leslie@gmail.com")
+// })
+
+// test("testing role", () => {
+
+// }
 
