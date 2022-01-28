7ce31d370568511978a4c48a24f92d234c021067
Leslie Patino <lesliejpatino@gmail.com>
2022-01-27 19:50:29 -0800

    ran all tests for the engineer class

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,9 +1,21 @@
 const Engineer = require("../lib/Engineer");
 
-test("testing engineers github", () => {
-    const testGithub = "octocat"
-    const githubInstance = new Engineer("", "octocat", "", testGithub) 
-    expect(githubInstance.github).toBe(testGithub)
-})
+// getGithub()
+// getRole()
 
-// test methods
+describe('Engineer', () => {
+    it('getGithub returns the employees github username', () => {
+
+        let employee = new Engineer("Leslie", "21", "leslie@coder.com", "octocat");
+        let employeeName = employee.getGithub();
+
+        expect(employeeName).toBe("octocat");
+    }) 
+    it('getRole returns the id of the current employee', () => {
+
+        let employee = new Engineer("Leslie", "21", "leslie@coder.com");
+        let employeeName = employee.getRole();
+
+        expect(employeeName).toBe("Engineer");
+    }) 
+});
