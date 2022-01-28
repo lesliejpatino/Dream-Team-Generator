35707c97c94744aa15f0ad85ec61e148a15ba48a
Leslie Patino <lesliejpatino@gmail.com>
2022-01-27 19:55:42 -0800

    ran all tests for the intern class

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,12 +1,21 @@
 const Intern = require("../lib/Intern");
 
-test("testing school", () => {
-    const testSchool = "UCB";
-    const schoolInstance = new Intern("","","", testSchool);
-    expect(schoolInstance.school).toBe(testSchool);
-});
+// getSchool() - passed
+// getRole - passed
 
-describe("Intern", () =>{
-    
-})
-// test method
+describe('Intern', () => {
+    it('getSchool returns the interns school name', () => {
+
+        let employee = new Intern("Leslie", "21", "leslie@coder.com", "UCB");
+        let employeeName = employee.getSchool();
+
+        expect(employeeName).toBe("UCB");
+    }) 
+    it('getRole returns the role of the current employee', () => {
+
+        let employee = new Intern("Leslie", "21", "leslie@coder.com", "UCB");
+        let employeeName = employee.getRole();
+
+        expect(employeeName).toBe("Intern");
+    }) 
+});
